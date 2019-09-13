25ea780b4645320074c66033642eacfcb1d662d1
Satish Ganesan <satishkumar.ganesan@gmail.com>
2019-09-13 13:05:34 +0100

    Initial kinesis

---
+++ terraform/kinesis/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_profile"  {}
+variable "aws_iam_profile"  {}
+variable "credentials_file" {}
+variable "resource_group" {}
+variable "region" {}
+variable "bucket_name" {}
+variable "code_bucket_name" {}
